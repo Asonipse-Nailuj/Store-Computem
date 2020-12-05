c7c7a0ae02bce8ba116cc1ea12ec9504b41f4e1f
Julian Espinosa <jaet1509@outlook.com>
2020-12-05 17:53:40 -0500

    Eliminacion de barra de progreso superior

--- login.php
+++ login.php
@@ -26,8 +26,6 @@ include_once("conexion.php");
   <link href="vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
   <!-- Font Awesome -->
   <link href="vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-  <!-- NProgress -->
-  <link href="vendors/nprogress/nprogress.css" rel="stylesheet">
   <!-- Animate.css -->
   <link href="vendors/animate.css/animate.min.css" rel="stylesheet">
 
